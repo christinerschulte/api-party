51eb9fc2943dc4d11958dc034e0b505641e908ef
Christine Schulte <christinerschulte@gmail.com>
2018-05-29 15:24:20 -0400

    Add and style GitHub component

---
+++ src/Github.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+import './Github.css'
+
+class Github extends Component {
+    render(){
+        return(
+            <div className="Github">
+                <img
+                    src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+                    alt="GitHub"
+                    className="logo"
+                />
+                <form>
+                    <div>
+                        <input type="text"/>
+                    </div>
+                    <div>
+                        <button type="submit">Look up Github user</button>
+                    </div>
+                </form>
+            </div>
+
+        )
+    }
+}
+
+export default Github
